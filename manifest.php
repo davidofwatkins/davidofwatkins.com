9456f99ce999b83b00510e946f204178a242f398
David Watkins <dwat91@gmail.com>
2013-08-30 01:37:22 -0700

    Fixed typo on home page

--- manifest.php
+++ manifest.php
@@ -2,7 +2,7 @@
 
 	/* Settings and details for davidofwatkins.com */
 	
-    define(SITEVERSION, "1.1");
+    define(SITEVERSION, "1.1.1");
     define(SITEROOT, "http://beta.davidofwatkins.com/");
     
 ?>
