030a31f11de4ba66e22ba08a434ef7d1e53bbf6b
David Watkins <dwat91@gmail.com>
2013-10-02 15:01:22 -0700

    Changing SITEROOT back to beta.dav...

--- manifest.php
+++ manifest.php
@@ -3,6 +3,6 @@
 	/* Settings and details for davidofwatkins.com */
 	
     define(SITEVERSION, "1.1.2");
-    define(SITEROOT, "http://davidofwatkins.com/");
+    define(SITEROOT, "http://beta.davidofwatkins.com/");
     
 ?>
