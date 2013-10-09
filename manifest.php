b5090168c617ffcedd23d2cc3d7b9e26c6b51cf9
David Watkins <dwat91@gmail.com>
2013-10-08 17:47:26 -0700

    Updated manifest to automatically generate application root so there is no longer any need to adjust SITEROOT every time the site is published to davidofwatkins.com

--- manifest.php
+++ manifest.php
@@ -1,8 +1,13 @@
 <?php
 
 	/* Settings and details for davidofwatkins.com */
-	
-    define(SITEVERSION, "1.1.2");
-    define(SITEROOT, "http://beta.davidofwatkins.com/");
+
+	//Calculate the application root
+    $appRootURL  = isset($_SERVER['HTTPS']) ? 'https://' : 'http://';
+	$appRootURL .= $_SERVER['SERVER_NAME'];
+	$appRootURL .= htmlspecialchars($_SERVER['REQUEST_URI']);
+
+	define(SITEVERSION, "1.1.2");
+    define(SITEROOT, $appRootURL);
     
 ?>
