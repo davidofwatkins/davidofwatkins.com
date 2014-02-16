ba5fc918159ba9775fdffd9be31657376b947da9
David Watkins <dwat91@gmail.com>
2014-02-15 22:47:23 -0800

    Fixed formatting of mobile facebook/gplus social buttons; added GitHub link to mobile layout

--- includes/footer.php
+++ includes/footer.php
@@ -9,10 +9,11 @@
         </div>
 		<div id="randomfavquote"><?= getRandomFavQuote() ?></div>
 		<div id="likebuttons-mobile">
-			<fb:like href="http://davidofwatkins.com" send="false" layout="button_count" width="50" show_faces="false"></fb:like>
-			<g:plusone href="http://davidofwatkins.com" annotation="inline" width="120"></g:plus>
+			<div id="mobile-gplus-wrapper"><g:plusone href="http://davidofwatkins.com" annotation="inline" width="120"></g:plusone></div>
+			<div id="mobile-fb-wrapper"><fb:like href="http://davidofwatkins.com" send="false" layout="button_count" width="50" show_faces="false"></fb:like></div>
 		</div>
 		<div id="mobile-social-buttons">
+			<a href="https://github.com/davidofwatkins" target="_blank" ><img src="<?= SITEROOT ?>images/linkicons/github_off.png" /></a>
 			<a href="https://plus.google.com/104494880066441442910/about" target="_blank" ><img src="<?= SITEROOT ?>images/linkicons/googleplus_off.png" /></a>
             <a href="http://www.facebook.com/davidofwatkins" target="_blank" ><img src="<?= SITEROOT ?>images/linkicons/facebook_off.png" /></a>
             <a href="http://twitter.com/dwat91" target="_blank" ><img src="<?= SITEROOT ?>images/linkicons/twitter_off.png" /></a>
